339971e214ee6bf07bd3bc55aec77ee501a54535
felbgs <fel_bgs@hotmail.com>
2017-01-24 15:03:19 -0300

    Add files via upload

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/Login.java
@@ -0,0 +1,26 @@
+package br.com.alura.gerenciador.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import br.com.alura.gerenciador.Usuario;
+import br.com.alura.gerenciador.dao.UsuarioDAO;
+
+public class Login implements Tarefa {
+
+	@Override
+	public String executa(HttpServletRequest req, HttpServletResponse resp) {
+		String email = req.getParameter("email");
+		String senha = req.getParameter("senha");
+		Usuario usuario = new UsuarioDAO().buscaPorEmailESenha(email, senha);
+		//PrintWriter writer = resp.getWriter();
+		if (usuario == null) {
+			return "<html><body>Usuário inválido!</body></html>";
+		} else {
+			HttpSession session = req.getSession();
+			session.setAttribute("usuarioLogado", usuario);
+			return "<html><body>Usuário logado: </body></html>" + email;
+		}
+	}
+}
